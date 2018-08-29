6d611e8c925bb4c57156d4db249b21b36d63a3c6
Adams1991 <adams121@hotmail.co.uk>
2018-08-29 10:45:05 +0100

    setups gamebox and app.js

---
+++ src/containers/GameBox.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Board from '../components/Board.js';
+
+
+class GameBox {
+  constructor(props){
+    super(props);
+    this.state = {
+      board : [],
+      pictures: ['../../public/images/Craig.jpg','../../public/images/Jarrod.jpg']
+    };
+
+
+
+
+}
+
+export default GameBox;
